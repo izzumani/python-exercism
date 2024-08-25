104e22d306d596d222cde18297074041a2c5aa76
isaac njiru <isaac.njiru@gmail.com>
2024-08-25 00:37:32 -0600

    1. Create an inventory based on a list\n 2. Add items from a list to an existing dictionary\n 3. Decrement items from the inventory\n 4. Remove an entry entirely from the inventory\n 5. Return the entire content of the inventory\n

---
+++ inventory-management/dicts.py
@@ -0,0 +1,72 @@
+"""Functions to keep track and alter inventory."""
+
+
+def create_inventory(items):
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    :param items: list - list of items to create an inventory from.
+    :return: dict - the inventory dictionary.
+    """
+    inventory ={}
+    for value in items:
+        inventory[value]=inventory.get(value,0) + 1
+
+    return inventory
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return: dict - the inventory updated with the new items.
+    """
+
+    
+    for value in items:
+        inventory[value]=inventory.get(value,0) + 1
+
+    return inventory
+
+
+
+def decrement_items(inventory, items):
+    """Decrement items in inventory using elements from the `items` list.
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return: dict - updated inventory with items decremented.
+    """
+
+    for value in items:
+        
+        if inventory.get(value,0)  > 0:
+            inventory[value] -=  1
+    return inventory
+
+
+def remove_item(inventory, item):
+    """Remove item from inventory if it matches `item` string.
+
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return: dict - updated inventory with item removed. Current inventory if item does not match.
+    """
+
+    inventory.pop(item,'Not found')
+    return inventory
+
+
+def list_inventory(inventory):
+    """Create a list containing only available (item_name, item_count > 0) pairs in inventory.
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    new_inventory_list = list(inventory.items())
+
+    for index,tuple_value in enumerate(new_inventory_list):
+        if tuple_value[1]==0:
+            new_inventory_list.pop(index)
+
+    return new_inventory_list
